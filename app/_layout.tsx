70f63e8259be3aaf60103ae49408269708d0f554
leon smit <leoncsmit@gmail.com>
2026-03-24 10:10:15 +1100

    Added navigation and default screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { Slot, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+
 
 export default function RootLayout() {
-  return <Stack />;
+    const [onboarded, setOnboarded] = useState(false)
+    const router = useRouter();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (!onboarded) {
+                router.replace('/(onboarding)/signup');
+            } else {
+                router.replace('/(tabs)');
+            }
+        }, 0);
+
+        return () => clearTimeout(timer);
+    }, [onboarded]);
+
+  return <Slot />;
 }
