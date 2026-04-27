1d22a3e1ae4311dbefe99be0d1a4afadf279d26f
Leon <codetips4572@gmail.com>
2026-04-27 17:28:47 +1000

    Added context to keep track of current activity and activity data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,19 @@
+import { ActivityAttemptProvider } from '@/context/ActivityAttemptContext';
 import { UserProvider } from '@/context/UserContext';
 import useColorPalette from '@/hooks/useColorPalette';
-import { Slot, useRouter } from 'expo-router';
+import { Slot } from 'expo-router';
 import { View } from 'react-native';
 
 export default function RootLayout() {
-    const router = useRouter();
     const colors = useColorPalette()
 
   return(
-    <UserProvider>
-        <View style={{ backgroundColor: colors.background, flex: 1}}>
-            <Slot />
-        </View>
-    </UserProvider>
+    <ActivityAttemptProvider>
+      <UserProvider>
+          <View style={{ backgroundColor: colors.background, flex: 1}}>
+              <Slot />
+          </View>
+      </UserProvider>
+    </ActivityAttemptProvider>
   )
 }
